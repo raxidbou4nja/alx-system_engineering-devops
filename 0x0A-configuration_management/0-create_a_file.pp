f60d85c783180360612356a09c22e1c65f2526c1
raxidbou4nja <rachid.boughnja@gmail.com>
2023-12-22 07:47:08 -0800

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,6 @@
+#Using Puppet, create a file in /tmp
+
 file { '/tmp/school':
-  ensure  => file,         # Ensure it is a file
   mode    => '0744',       # Set file permission to 0744
   owner   => 'www-data',   # Set file owner to www-data
   group   => 'www-data',   # Set file group to www-data
