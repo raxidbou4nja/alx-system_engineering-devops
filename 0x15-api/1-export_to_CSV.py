79710e5310a3d08ce6d858b846d5907784031dea
raxidbou4nja <raxid.bou4nja@gmail.com>
2024-01-29 14:41:21 -0800

    add revert changes

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,37 +1,21 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
-import csv
-import requests
-from sys import argv
-
-
-def to_csv():
-    """return API data"""
-    users = requests.get("http://jsonplaceholder.typicode.com/users")
-    for user in users.json():
-        if user.get('id') == int(argv[1]):
-            USERNAME = (user.get('username'))
-            break
-    TASK_STATUS_TITLE = []
-
-    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
-    for task in todos.json():
-        if task.get('userId') == int(argv[1]):
-            TASK_STATUS_TITLE.append((task.get('completed'),
-                                      task.get('title')))
-
-    """export to csv"""
-    filename = "{}.csv".format(argv[1])
-    with open(filename, "w") as csvfile:
-        fieldnames = ["USER_ID", "USERNAME",
-                      "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
-                                quoting=csv.QUOTE_ALL)
-        for task in TASK_STATUS_TITLE:
-            writer.writerow({"USER_ID": argv[1], "USERNAME": USERNAME,
-                             "TASK_COMPLETED_STATUS": task[0],
-                             "TASK_TITLE": task[1]})
-
+"""Exports data in the CSV format"""
 
 if __name__ == "__main__":
-    to_csv()
+
+    import csv
+    import requests
+    import sys
+
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(sys.argv[1]))
+    name = user.json().get('username')
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+
+    with open(sys.argv[1] + '.csv', mode='w') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL, lineterminator='\n')
+        for task in todos.json():
+            if task.get('userId') == int(sys.argv[1]):
+                writer.writerow([sys.argv[1], name, str(task.get('completed')),
+                                 task.get('title')])
