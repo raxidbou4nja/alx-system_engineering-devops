dd7709cc873579fda99dbdbb9b91cb372c20e494
raxidbou4nja <raxid.bou4nja@gmail.com>
2024-01-29 14:46:02 -0800

    fix files

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,37 +1,19 @@
 #!/usr/bin/python3
-"""Export to CSV"""
-import csv
+"""Exports to-do list information for a given employee ID to JSON format."""
+import json
 import requests
-from sys import argv
+import sys
 
-def export_to_csv(user_id):
-    # Define the URLs for user and todo data
-    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    try:
-        # Fetch user and todo data
-        user_response = requests.get(user_url)
-        todo_response = requests.get(todo_url)
-
-        user_data = user_response.json()
-        todo_data = todo_response.json()
-
-        # Prepare CSV data
-        csv_data = [[user_data['id'], user_data['username'], task['completed'], task['title']] for task in todo_data]
-
-        # Write CSV data to file
-        with open(f"{user_id}.csv", 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(csv_data)
-
-        print(f'Data exported to {user_id}.csv')
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-
-if __name__ == '__main__':
-    if len(argv) != 2:
-        print("Usage: {} <user_id>".format(argv[0]))
-    else:
-        export_to_csv(argv[1])
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
