9e745442912381ccea6014e4b87a6278011d08a4
raxidbou4nja <raxid.bou4nja@gmail.com>
2024-01-29 12:34:26 -0800

    update file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,41 +1,24 @@
 #!/usr/bin/python3
-
-"""
-Python script that exports data in the JSON format.
-"""
-
-from requests import get
-from sys import argv
+"""Export to JSON"""
 import json
+import requests
+from sys import argv
 
-if __name__ == "__main__":
-    response = get('https://jsonplaceholder.typicode.com/todos/')
-    data = response.json()
 
-    row = []
-    response2 = get('https://jsonplaceholder.typicode.com/users')
-    data2 = response2.json()
+if __name__ == '__main__':
+    BASE_URL = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+    BASE_TODO = f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos"
 
-    for i in data2:
-        if i['id'] == int(argv[1]):
-            u_name = i['username']
-            id_no = i['id']
+    user = requests.get(BASE_URL)
+    todo = requests.get(BASE_TODO)
+    lis = []
 
-    row = []
+    for i in todo.json():
+        new = {'task': f"{i.get('title')}",
+               'completed': i.get('completed'),
+               'username': f"{user.json().get('username')}"}
+        lis.append(new)
 
-    for i in data:
-
-        new_dict = {}
-
-        if i['userId'] == int(argv[1]):
-            new_dict['username'] = u_name
-            new_dict['task'] = i['title']
-            new_dict['completed'] = i['completed']
-            row.append(new_dict)
-
-    final_dict = {}
-    final_dict[id_no] = row
-    json_obj = json.dumps(final_dict)
-
-    with open(argv[1] + ".json",  "w") as f:
-        f.write(json_obj)
+        dic = {f"{argv[1]}": lis}
+        with open(f"{argv[1]}.csv", 'w') as file:
+            json.dump(dic, file)
