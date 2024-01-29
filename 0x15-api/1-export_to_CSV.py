5be36a12267954189b08565b50826b73816e36c7
raxidbou4nja <raxid.bou4nja@gmail.com>
2024-01-29 12:26:17 -0800

    fix errors

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,57 +1,41 @@
 #!/usr/bin/python3
 
 """
-Python script that exports data in the CSV format.
+Python script that exports data in the JSON format.
 """
 
-import csv
 from requests import get
 from sys import argv
-
-
-def get_user_info(user_id):
-    response = get(f'https://jsonplaceholder.typicode.com/users/{user_id}')
-    return response.json()
-
-
-def get_todo_list(user_id):
-    r = get(f'https://jsonplaceholder.typicode.com/todos?userId={user_id}')
-    return r.json()
-
-
-def export_to_csv(user_id, user_info, todo_list):
-    filename = f'{user_id}.csv'
-    with open(filename, mode='w', newline='') as csv_file:
-        f = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-        writer = csv.DictWriter(csv_file, fieldnames=f)
-
-        writer.writeheader()
-
-        for task in todo_list:
-            writer.writerow({
-                'USER_ID': user_id,
-                'USERNAME': user_info['username'],
-                'TASK_COMPLETED_STATUS': str(task['completed']),
-                'TASK_TITLE': task['title']
-            })
-
-
-def main():
-    try:
-        if len(argv) != 2:
-            raise ValueError("Usage: {} <user_id>".format(argv[0]))
-
-        user_id = int(argv[1])
-        user_info = get_user_info(user_id)
-        todo_list = get_todo_list(user_id)
-
-        export_to_csv(user_id, user_info, todo_list)
-
-    except ValueError as ve:
-        print(f"Error: {ve}")
-    except Exception as e:
-        print(f"Error: {e}")
-
+import json
 
 if __name__ == "__main__":
-    main()
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
+
+    row = []
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
+
+    for i in data2:
+        if i['id'] == int(argv[1]):
+            u_name = i['username']
+            id_no = i['id']
+
+    row = []
+
+    for i in data:
+
+        new_dict = {}
+
+        if i['userId'] == int(argv[1]):
+            new_dict['username'] = u_name
+            new_dict['task'] = i['title']
+            new_dict['completed'] = i['completed']
+            row.append(new_dict)
+
+    final_dict = {}
+    final_dict[id_no] = row
+    json_obj = json.dumps(final_dict)
+
+    with open(argv[1] + ".json",  "w") as f:
+        f.write(json_obj)
