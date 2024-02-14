230dc4b6f9bc409a9151c9290d858850d3adcd4d
raxidbou4nja <rachid.boughnja@gmail.com>
2024-02-14 02:01:24 +0000

    update code

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,6 @@ of the first 10 hot posts for a given subreddit.
 """
 import requests
 
-
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
@@ -15,10 +14,12 @@ def top_ten(subreddit):
     params = {
         "limit": 10
     }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+
     if response.status_code == 404:
         print("None")
         return
+
     results = response.json().get("data")
     [print(c.get("data").get("title")) for c in results.get("children")]
