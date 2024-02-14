3a5b9826c53c3df7e1cdd5506fe663e24671bc8a
raxidbou4nja <125773922+raxidbou4nja@users.noreply.github.com>
2024-02-14 20:14:12 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,19 @@
-#!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
-
 import requests
-
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': 'by /u/factos22'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'by /u/factos22'}
+
+    try:
+        r = requests.get(url, headers=headers)
+        r.raise_for_status()
+        data = r.json()
+        subs = data.get("data", {}).get("subscribers", 0)
+        return subs
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
