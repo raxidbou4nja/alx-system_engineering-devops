7ea4f15d26092dedf4a3644908ec0fe5302dcaf4
raxidbou4nja <125773922+raxidbou4nja@users.noreply.github.com>
2024-02-14 20:16:36 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
+""" a program that queries Reddit Api and returns
+the number of subscribers """
 
 import requests
 
-def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit"""
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-    
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'by /u/factos22'}
+Base_url = 'https://www.reddit.com'
 
-    try:
-        r = requests.get(url, headers=headers)
-        r.raise_for_status()
-        data = r.json()
-        subs = data.get("data", {}).get("subscribers", 0)
-        return subs
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return None
+
+def number_of_subscribers(subreddit):
+    """ a function that queries reddit's api"""
+
+    api_headers = {
+         'Accept': 'application/json',
+         'user-agent': ' '.join([
+             'Mozilla/5.0 (Linux; Android 6.0;Nexus 5 Build/MRA58N)'
+             'AppleWebKit/537.36 (KHTML, like Gecko)'
+             'Chrome/106.0.0.0 Mobile Safari/537.36'
+         ])
+        }
+
+    res = requests.get(
+        '{}/r/{}/about/.json'.format(Base_url, subreddit),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
+    return 0
