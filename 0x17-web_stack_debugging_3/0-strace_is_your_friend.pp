5734dacdf7919aa50b21b27ccd391298950b65bb
raxidbou4nja <125773922+raxidbou4nja@users.noreply.github.com>
2024-02-14 21:42:05 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,6 @@
 # Fix bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
 exec { 'fix-wordpress':
-  command     => '/bin/true',
-  path        => '/usr/local/bin/:/bin/',
-  refreshonly => true,
-  subscribe   => File['/var/www/html/wp-settings.php'],
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
